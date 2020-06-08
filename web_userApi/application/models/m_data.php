cd8e2ac7700073682b2bb98521ec6b9f9f345be7
E41181181 <56006056+E41181181@users.noreply.github.com>
2020-06-08 22:22:30 +0700

    webuserAPI

---
+++ web_userApi/application/models/m_data.php
@@ -0,0 +1,34 @@
+<?php 
+
+class M_data extends CI_Model{
+	function tampil_data(){
+		return $this->db->get('user');
+	}
+
+	function input_data($data,$table){
+		$this->db->insert($table,$data); //masukkan data ke database
+	}
+
+	function tampil_surat(){		
+		$nim=$_SESSION["nim"];
+
+		$this->db->order_by('ID_SURAT', 'DESC');
+		$this->db->where('NIM',$nim);
+        return $query = $this->db->from('surat')
+		  ->join('jenis_surat', 'jenis_surat.ID_JENIS_SURAT=surat.ID_JENIS_SURAT')
+		->get();
+	}	
+
+	function Tampil_surat2($status) 
+    {
+		$nim=$_SESSION["nim"];
+		// print_r($status); untuk menampilkan $status
+
+		$this->db->order_by('ID_SURAT', 'DESC'); //untuk mengurut data sql berdasarkan id surat
+		$this->db->where('NIM',$nim); //pilih data spesidik berdasarkan nim
+		$this->db->where('STATUS_SURAT',$status); //pilih bdata spesifik berdasarkan status surat
+        return $query = $this->db->from('surat') //perintah join sql di ci
+		  ->join('jenis_surat', 'jenis_surat.ID_JENIS_SURAT=surat.ID_JENIS_SURAT')
+		->get(); //dapatkan hasil datanya
+    } 
+}
