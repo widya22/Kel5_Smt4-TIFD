e132aa58c4f2cd4aa1c01b5ffd4c6bf8cdf02b2a
E41181181 <56006056+E41181181@users.noreply.github.com>
2020-07-03 05:08:03 +0700

    Update suratPending.php

--- suratjti/application/views/suratPending.php
+++ suratjti/application/views/suratPending.php
@@ -25,7 +25,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index.php" class="brand-link">
+    <a href="#" class="brand-link">
       <img src="<?php echo base_url('assets/asetadmin/dist/img/jti.png');?>" alt="logo jti" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Surat JTI</span>
